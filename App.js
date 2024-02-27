ae03c46c77a553ac0d2b43479701cd48e4283758
Sponton <134696651+sofisponton@users.noreply.github.com>
2024-02-27 12:21:41 +0000

    second commit

--- App.js
+++ App.js
@@ -4,7 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Etec Professor Milton Gazzetti</Text>
+      <Text style={{ fontStyle: 'italic' }}>3°Ano</Text>
+      <Text style={{textDecorationLine: 'line-through'}}>Informática</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +18,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  
   },
 });
